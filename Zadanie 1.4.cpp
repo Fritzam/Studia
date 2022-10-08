91f585afd86c90dcc2555df4578dc15156a22011
Tornak9 <77358377+Tornak9@users.noreply.github.com>
2022-10-08 13:18:33 +0200

    Zadanie 1.4

---
+++ Zadanie 1.4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cout << "Wprowadź liczbę x" << endl;
+    cin >> x;
+    cout << "Wprowadź liczbę y" << endl;
+    cin >> y;
+    cout << "Wprowadź liczbę z" << endl;
+    cin >> z;
+    if (x > y && x > z) {
+        cout << "Największa jest liczba x wynosząca: " << x << endl;
+    }
+    if (y > x && y > z) {
+        cout << "Największa jest liczba y wynosząca: " << y << endl;
+    }
+    if (z > x && z > y) {
+        cout << "Największa jest liczba z wynosząca: " << z << endl;
+    }
+    return 0;
+    }
