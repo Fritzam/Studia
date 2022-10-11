ad3d2a398e8601bf9cb09d01ba0666fda64727c7
Tornak9 <Tornak9@gmail.com>
2022-10-11 02:34:23 +0200

    Add ex 1.3 to pprg 08-09.10.22

---
+++ CwiczeniaPPRG1Semestr/Zjazd08-09.10.22r.PD/Zadanie 1.3/Zadanie_1.3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Pobranie wartości od użytkownika.
+    int liczba;
+    cout << "Proszę podać liczbę: " << endl;
+    cin >> liczba;
+
+    //Sprawdzenie czy liczba jest parzysta, nieparzysta, lub żadna.
+    if (liczba == 0) {
+        cout << "Liczba jest zerem, lub wartością nieprzewidzianą przez twórcę programu." << endl;
+    }
+    else if (liczba % 2 != 0) {
+        cout << "Liczba " << liczba << " jest liczbą nieparzystą." << endl;
+    }
+    else {
+        cout << "Liczba " << liczba << " jest liczbą parzystą." << endl;
+    }
+    return 0;
+}
