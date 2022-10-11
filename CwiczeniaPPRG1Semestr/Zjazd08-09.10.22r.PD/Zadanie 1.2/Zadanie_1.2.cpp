3e3ec41efb974c46c988bbe1a9c50f0d9f0a2427
Tornak9 <Tornak9@gmail.com>
2022-10-11 02:19:02 +0200

    Add ex 1.2 to pprg 08-09.10.22

---
+++ CwiczeniaPPRG1Semestr/Zjazd08-09.10.22r.PD/Zadanie 1.2/Zadanie_1.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Pobranie liczby od użytkownika.
+    int liczba;
+    cout << "Proszę podać liczbę: " << endl;
+    cin >> liczba;
+
+    //Sprawdzenie czy liczba jest liczbą dodatnią, zerem(inną wartością), lub liczbą ujemną.
+    if (liczba > 0) {
+        cout << "Liczba " << liczba << " jest liczbą dodatnią." << endl;
+    }
+    else if (liczba == 0) {
+        cout << "Liczba jest zerem, lub wartością nieprzewidzianą przez twórcę programu." << endl;
+    }
+    else if (liczba < 0){
+        cout << "Liczba " << liczba << " jest liczbą ujemną." << endl;
+    }
+    return 0;
+}
