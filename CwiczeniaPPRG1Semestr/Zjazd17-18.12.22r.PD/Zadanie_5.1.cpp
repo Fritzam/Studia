7ceaf7d3e0db4581c88a8f8a630ddb287bcc0475
Michał Fritza <s29235@szuflandia22.pjwstk.edu.pl>
2022-12-17 12:53:24 +0100

    Working ex 5.1

---
+++ CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_5.1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    //Deklaracja zmiennych
+    int ilosc_elementow;
+    int element;
+    int najwiekszy_element;
+    int indeks_największego_elementu;
+    
+    //Pozyskanie od użytkownika ilości elementów wektora.
+    cout << "Proszę podać ilość elementów wektora: " << endl;
+    cin >> ilosc_elementow;
+
+    //Zainicjalizowanie wektora.
+    vector <int> wektor(ilosc_elementow);
+
+    //Pozyskanie elementu od użytkownika i wstawienie go do wektora.
+    for (int i = 0; i < ilosc_elementow; i++) {
+        cout << "Proszę podać " << i + 1 << " element." << endl;
+        cin >> element;
+        wektor[i] = element;
+    };
+
+    //Przypisanie największemu elementowi wartości początkowej.
+    najwiekszy_element = wektor[0];
+
+    //Odnalezienie największego elementu wektora.
+    for (int i = 0; i < wektor.size(); i++) {
+        if (wektor[i] > najwiekszy_element) {
+            najwiekszy_element = wektor[i];
+        }
+    };
+
+    //Odnalezienie indeksu największego elementu.
+    for (int i = 0; i < wektor.size(); i++) {
+        if (wektor[i] == najwiekszy_element) {
+            indeks_największego_elementu = i;
+        }
+    };
+
+    cout << "Największy element wynosi: " << najwiekszy_element << endl;
+    cout << "Indeks największego elementu to: " << indeks_największego_elementu << endl;
+
+    return 0;
+}
