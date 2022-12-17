462b512d8bc7dcd00f4fb9c1cfcd9522bc455934
Michał Fritza <s29235@szuflandia22.pjwstk.edu.pl>
2022-12-17 13:25:03 +0100

    Unfinished ex 5.2

---
+++ CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_5.2/Zadanie_5.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int liczba_wierszy;
+    int liczba_kolumn;
+    
+    cout << "Proszę podać ilość wierszy w tablicy:" << endl;
+    cin >> liczba_wierszy;
+    cout << "Proszę podać ilość kolumn: " << endl;
+    cin >> liczba_kolumn;
+
+    
+
+
+    return 0;
+}
