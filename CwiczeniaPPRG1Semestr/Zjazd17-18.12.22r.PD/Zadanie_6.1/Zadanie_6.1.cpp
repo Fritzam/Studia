d8b4b9a043aab94258c15c23f9180ca26f28aad5
Michal Fritza <Tornak9@gmail.com>
2022-12-22 07:51:23 +0100

    Working ex 6.1

--- CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_6.1/Zadanie_6.1.cpp
+++ CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_6.1/Zadanie_6.1.cpp
@@ -3,22 +3,42 @@
 
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Proszę podać liczbę do której ma być generowany program." >> endl;
-    cin >> n;
-    vector <int> liczby;
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if ((i % 3 != 0) && (i % 5 == 0)) {
-                liczby.push_back(j);
-            }
-
+int sprawdzenie_ilosci_liczb(int n) {
+    int liczba = 0;
+    for (int i = 5; i < n; i += 5) {
+        if (i % 3 != 0) {
+            liczba++;
         }
     }
 
-    
+    return liczba;
+}
+
+void wyswietlenie_poszczegolnych_liczb(int n) {
+    vector <int> przez_piec;
+    for (int i = 5; i < n; i += 5) {
+        if (i % 3 != 0) {
+            przez_piec.push_back(i);
+        }
+    }
+
+    for (int i = 0; i < przez_piec.size(); i++) {
+        cout << i+1 << " element podzielny przez 5, lecz niepodzielny przez 3 to : " << przez_piec[i] << endl;
+    }
+}
+
+
+
+
+int main() {
+    int n;
+    cout << "Proszę podać liczbę." << endl;
+    cin >> n;
+
+    wyswietlenie_poszczegolnych_liczb(n);
+    cout << "-----------------------------------------------------------" << endl;
+    cout << "Ilość liczb podzielnych przez 5, lecz niepodzielnych przez 3 mniejszych niż " << n << " wynosi: " << sprawdzenie_ilosci_liczb(n) << endl;
+    cout << "-----------------------------------------------------------" << endl;
 
     return 0;
 }
