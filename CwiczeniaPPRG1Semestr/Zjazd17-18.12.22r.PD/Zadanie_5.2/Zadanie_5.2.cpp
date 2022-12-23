0d33cca7cc0a4560d46d03244511b1005ae48e6e
Michal Fritza <Tornak9@gmail.com>
2022-12-23 04:53:11 +0100

    Add comments to ex 5.2

--- CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_5.2/Zadanie_5.2.cpp
+++ CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_5.2/Zadanie_5.2.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-
+//Funkcja dodajaca podane przez użytkownika elementy do wektora.
 void add_to_vector(vector <int>& wektor, int liczba_w_tablicy, int& licznik) {
     for (int i = 0; i < liczba_w_tablicy; i++) {
         int element;
@@ -13,6 +13,8 @@ void add_to_vector(vector <int>& wektor, int liczba_w_tablicy, int& licznik) {
     }
     licznik++;
 }
+
+//Funkcja wyświetlająca elementy wektora na ekranie.
 void display_vector(vector <int> wektor1, vector <int> wektor2) {
 
     for (int i = 0; i < wektor1.size(); i++) {
@@ -20,6 +22,8 @@ void display_vector(vector <int> wektor1, vector <int> wektor2) {
     }
 
 }
+
+//Funkcja podmieniająca odpowiadające sobie elementy wektorów tak że wek1[0] | wek2[0] --> swap --> wek2[0] | wek1[0] itd.
 void swap_vectors(vector <int>& wektor1, vector <int>& wektor2) {
     for (int i = 0; i < wektor1.size(); i++) {
         int swap_variable;
@@ -31,16 +35,19 @@ void swap_vectors(vector <int>& wektor1, vector <int>& wektor2) {
 
 }
 int main() {
+    //Deklaracja liczb całkowitych.
     int liczba_elementow_tablicy;
     int licznik_uzyc_funkcji = 1;
 
+    //Pobranie od użytkownika liczby elementów w tablicach.
     cout << "Podaj liczbę elementów tablic: " << endl;
     cin >> liczba_elementow_tablicy;
 
-
+    //Deklaracja wektorów.
     vector <int> wektor1;
     vector <int> wektor2;
 
+    //Wywołanie funkcji.
     add_to_vector(wektor1, liczba_elementow_tablicy, licznik_uzyc_funkcji);
     add_to_vector(wektor2, liczba_elementow_tablicy, licznik_uzyc_funkcji);
     cout << "-----------------------------------------" << endl;
