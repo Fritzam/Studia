254f9c3347afadfcf1fa765fc364061d1ee8b5df
Michal Fritza <Tornak9@gmail.com>
2022-12-23 05:11:42 +0100

    Separate 1 mega-function into 2 smaller, add comments.

--- CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_6.1/Zadanie_6.1.cpp
+++ CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_6.1/Zadanie_6.1.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//Funkcja zliczająca ilość liczb od 5 do n ktore są podzielne przez 5, lecz nie przez 3.
 int sprawdzenie_ilosci_liczb(int n) {
     int liczba = 0;
     for (int i = 5; i < n; i += 5) {
@@ -14,28 +15,41 @@ int sprawdzenie_ilosci_liczb(int n) {
     return liczba;
 }
 
-void wyswietlenie_poszczegolnych_liczb(int n) {
-    vector <int> przez_piec;
+//Funkcja wyświetlająca te liczby na standard output.
+void wyswietlenie_poszczegolnych_liczb(vector <int> wektor) {
+    for (int i = 0; i < wektor.size(); i++) {
+        cout << i+1 << " element podzielny przez 5, lecz niepodzielny przez 3 to : " << wektor[i] << endl;
+    }
+}
+
+//Funkcja dodająca te liczby do wektora.
+void dodanie_do_wektora(vector <int>& wektor, int n) {
     for (int i = 5; i < n; i += 5) {
         if (i % 3 != 0) {
-            przez_piec.push_back(i);
+            wektor.push_back(i);
         }
     }
 
-    for (int i = 0; i < przez_piec.size(); i++) {
-        cout << i+1 << " element podzielny przez 5, lecz niepodzielny przez 3 to : " << przez_piec[i] << endl;
-    }
 }
 
 
 
 
 int main() {
+
+    //Pozyskanie zasięgu sprawdzanych liczb i zapisanie go do zmiennej.
     int n;
-    cout << "Proszę podać liczbę." << endl;
+    cout << "Proszę podać liczbę do której mam sprawdzić liczby podzielne przez 5, lecz niepodzielne przez 3." << endl;
     cin >> n;
 
-    wyswietlenie_poszczegolnych_liczb(n);
+    //Deklaracja wektora.
+    vector <int> przez_piec;
+
+    //Wywołanie funkcji.
+    dodanie_do_wektora(przez_piec, n);
+    wyswietlenie_poszczegolnych_liczb(przez_piec);
+
+    //Wypisanie podsumowania na ekranie.
     cout << "-----------------------------------------------------------" << endl;
     cout << "Ilość liczb podzielnych przez 5, lecz niepodzielnych przez 3 mniejszych niż " << n << " wynosi: " << sprawdzenie_ilosci_liczb(n) << endl;
     cout << "-----------------------------------------------------------" << endl;
