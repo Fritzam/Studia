abe1158c830afc00e615e87894673edd3d7117ba
Michal Fritza <Tornak9@gmail.com>
2022-12-23 06:25:10 +0100

    Added comments to ex 6.4

--- CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_6.4/Zadanie_6.4.cpp
+++ CwiczeniaPPRG1Semestr/Zjazd17-18.12.22r.PD/Zadanie_6.4/Zadanie_6.4.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+/*Funkcja przyjmująca od użytkownika ciąg znaków i ilość powtórzeń, łącząca ciąg znaków n razy ze sobą
+    i wyświetlająca rezultat w terminalu. */
 void concat(int n, string ciag_znakow) {
     string concat_ciag_znakow;
     for (int i = 0; i < n; i++) {
@@ -16,15 +18,19 @@ void concat(int n, string ciag_znakow) {
 
 int main() {
 
+    //Deklaracja zmiennych.
     string ciag_znakow;
     int n;
 
+    //Prośba o ciąg znaków.
     cout << "Proszę podać ciąg znaków do konkatenacji: " << endl;
     cin >> ciag_znakow;
 
+    //Prośba o ilość konkatenacji.
     cout << "Proszę podać liczbę konkatenacji: " << endl;
     cin >> n;
 
+    //Wywołanie funkcji z powyższymi zmiennymi.
     concat(n, ciag_znakow);
 
     return 0;
