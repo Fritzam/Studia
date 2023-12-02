d31275cf07a6f33e914c21b688bedf22e3c7b031
Michal Fritza <fritzam96@gmail.com>
2023-12-02 17:32:55 +0100

    Added required test cases to MPR project

--- Metody_Programowania/WypozyczalnaMain/src/test/java/org/example/RentalServiceTest.java
+++ Metody_Programowania/WypozyczalnaMain/src/test/java/org/example/RentalServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -80,4 +81,71 @@ class RentalServiceTest {
 
         assertThat(overlappingDate).isTrue();
     }
+
+    @Test
+    void shouldOverlapFromRight() {
+        //Given
+        Rental rental = new Rental(null, null, LocalDate.now(), LocalDate.now().plusDays(4));
+        //When
+        boolean overlappingDateFromRight = rentalService.isOverlappingDate(LocalDate.now().plusDays(3), LocalDate.now().plusDays(7), rental);
+        //Then
+        assertThat(overlappingDateFromRight).isTrue();
+    }
+
+    @Test
+    void shouldCost5000InPremium() {
+        //Given
+        Car car = new Car("make", "model", "some-vin", Klasa.PREMIUM);
+        carStorage.add(car);
+        //When
+        double price = rentalService.estimatePrice(car.getVin(), LocalDate.now(), LocalDate.now().plusDays(10));
+        //Then
+        assertThat(price).isEqualTo(5000);
+    }
+
+    @Test
+    void shouldCost750InStandard() {
+        //Given
+        Car car = new Car("make", "model", "some-vin", Klasa.STANDARD);
+        carStorage.add(car);
+        //When
+        double price = rentalService.estimatePrice("some-vin", LocalDate.now(), LocalDate.now().plusDays(1));
+        //Assert
+        assertThat(price).isEqualTo(750);
+    }
+
+    @Test
+    void shouldCost3000InSport() {
+        //Given
+        Car car = new Car("make", "model", "some-vin", Klasa.SPORT);
+        carStorage.add(car);
+        //When
+        double price = rentalService.estimatePrice("some-vin", LocalDate.now(), LocalDate.now().plusDays(2));
+        //Then
+        assertThat(price).isEqualTo(3000);
+    }
+
+    @Test
+    void shouldReturnRental() {
+        //Given
+        Car car = new Car("make", "model", "some-vin", Klasa.PREMIUM);
+        carStorage.add(car);
+        //When
+        Rental rental = rentalService.rent(1, "some-vin", LocalDate.now(), LocalDate.now().plusDays(2));
+        //Then
+        assertThat(rental).isInstanceOf(Rental.class);
+    }
+
+    @Test
+    void shouldReturnNull() {
+        //Given
+        Car car = new Car("marka", "model", "some-vin", Klasa.PREMIUM);
+        carStorage.add(car);
+        //When
+        rentalService.rent(1, "some-vin", LocalDate.now().plusDays(1), LocalDate.now().plusDays(5));
+        Rental testRental = rentalService.rent(1, "some-vin", LocalDate.now().plusDays(1), LocalDate.now().plusDays(3));
+        //Then
+        assertThat(testRental).isNull();
+    }
+
 }
